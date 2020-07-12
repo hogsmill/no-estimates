489b64e62dbf7fbc9c660b3a25a5f4a01da24fa4
hogsmill <info@hogsmill.com>
2020-07-12 17:15:27 +0100

    Update about

--- src/components/about/AboutView.vue
+++ src/components/about/AboutView.vue
@@ -8,11 +8,17 @@
       <li><strong>Pull a Card into first queue</strong>. Click on "Work Cards" to pull the next card into the Design queue. Commit day is added automatically</li>
       <li><strong>Assign a Dependent Team</strong>. Click on the "Team" area in a card to set the dependent team (instead of picking up a dependent team card)</li>
       <li><strong>Bring up an event card</strong>. Not 100% sure how event cards work, so this just brings up the next one, and increments</li>
-      <li><strong>Day increment</strong> Click next to move the day on; we can hook in here to do blocking, etc. </li>
+      <li><strong>Day increment</strong> Click next to move the day on; it shows the next event card (should this be random?) and randoly blocks some cards </li>
       <li><strong>Pick Team name</strong> once selected, team members see state changes for that team </li>
       <li><strong>Pick team member name</strong> Once you set your name, your available effort appear top right. This is updated to 4 each day (although you can't see this yet) </li>
     </ul>
     <p>And obviously all the data structures behind all this, e.g. Cards have effort, urgent, team, etc. defined</p>
 
+    <p>Next steps...</p>
+    <ul class="text-left">
+      <li>Add names to teams</li>
+      <li>Assign effort - rules for other teams, rues for non-specialist, etc.</li>
+      <li>Create and populate the graphs, etc.</li>
+    </ul>
   </div>
 </template>

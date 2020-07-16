5dddde7f92302f517c5e7adadc08cd8d39bc1d5e
hogsmill <info@hogsmill.com>
2020-07-16 17:56:24 +0100

    scss for other team cards

--- src/components/board/OtherTeams.vue
+++ src/components/board/OtherTeams.vue
@@ -35,19 +35,70 @@ export default {
 }
 </script>
 
-<style>
-  .other-team-header { font-weight: bold; width: 100%; margin-top: 20px; }
+<style lang="scss">
+  .other-team-header {
+    font-weight: bold;
+    width: 100%;
+    margin-top: 20px;
+  }
 
-  .other-work-card { background-color: #fff; color: #444; margin: 6px; width: 122px; height: 72px; }
+  .other-work-card {
+    background-color: #fff;
+    color: #444;
+    margin: 6px;
+    width: 122px;
+    height: 72px;
 
-  .other-work-card .other-work-card-header { padding: 2px 2px 0 0; font-weight: bold; margin-bottom: 4px; }
-  .other-work-card .other-work-card-header .card-team { text-align: right; width: 80%; display: inline-block; }
-  .other-work-card .other-work-card-header .card-team span { padding: 2px; color: #fff; }
-  .other-work-card .other-work-card-header .card-number { text-align: left; width: 20%; padding-left: 2px; display: inline-block; }
+    .other-work-card-header {
+      padding: 2px 2px 0 0;
+      font-weight: bold;
+      margin-bottom: 4px;
 
-  .other-work-card .other-work-card-effort { text-align: left; }
-  .other-work-card .other-work-card-effort .other-work-card-column.column { background-color: #555; color: #fff; border: 0; display: inline-block; width: 11px; height: 11px; font-size: 8px; padding-left: 2px; }
-  .other-work-card .other-work-card-effort .assigned { background-color: #000; }
-  .other-work-card .other-work-card-effort .other-work-card-column { display: inline-block; border: 1px solid; margin-left: 1px; width: 8px; height: 8px; font-size: 8px; padding-left: 2px; }
+      .card-team {
+        text-align: right;
+        width: 80%;
+        display: inline-block;
 
+        span {
+          padding: 2px; color: #fff;
+        }
+      }
+
+      .card-number {
+        text-align: left;
+        width: 20%;
+        padding-left: 2px;
+        display: inline-block;
+      }
+    }
+
+    .other-work-card-effort {
+      text-align: left;
+
+      .other-work-card-column {
+        display: inline-block;
+        border: 1px solid;
+        margin-left: 1px;
+        width: 8px;
+        height: 8px;
+        font-size: 8px;
+        padding-left: 2px;
+
+        &.column {
+          background-color: #555;
+          color: #fff;
+          border: 0;
+          display: inline-block;
+          width: 11px;
+          height: 11px;
+          font-size: 8px;
+          padding-left: 2px;
+        }
+      }
+
+      .assigned {
+        background-color: #000;
+      }
+    }
+  }
 </style>

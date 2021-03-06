35313f56e6e1d16e5941c36ca8e368442cf19d7f
hogsmill <info@hogsmill.com>
2021-03-06 10:22:02 +0000

    mobile view

--- src/components/HostFunctions.vue
+++ src/components/HostFunctions.vue
@@ -3,6 +3,9 @@
     <button class="btn btn-sm btn-outline-secondary smaller-font clear-local-storage" @click="clearLocalStorage()">
       Clear Local Storage
     </button>
+    <button class="btn btn-sm btn-outline-secondary smaller-font clear-local-storage" @click="goMobile()">
+      Go Mobile
+    </button>
     <div class="run-demo rounded">
       <div class="expand">
         <i v-if="showRunGame" @click="setShowRunGame(false)" title="collapse" class="fas fa-caret-up toggle" />
@@ -103,6 +106,9 @@ export default {
         localStorage.removeItem('myRole')
         localStorage.removeItem('gameName')
       }
+    },
+    goMobile() {
+      this.$store.dispatch('updateCurrentTab', 'mobile')
     },
     setStepThrough() {
       const stepThrough = document.getElementById('step-through').checked

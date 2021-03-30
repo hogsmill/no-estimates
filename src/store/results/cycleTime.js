3e0c911c77fd11dafc8d682f1b6c25a1362e675d
hogsmill <info@hogsmill.com>
2021-03-30 09:43:47 +0100

    fix cycle time

--- src/store/results/cycleTime.js
+++ src/store/results/cycleTime.js
@@ -4,18 +4,30 @@ const cardFuns = require('../lib/cards.js')
 module.exports = {
 
   run: function(workCards) {
-    const results = {
+
+    let i, results = []
+    for (i = 0; i < workCards.length; i++) {
+      if (workCards[i].delivery) {
+        results.push({
+          id: '#' + workCards[i].number,
+          effort: cardFuns.totalEffort(workCards[i]),
+          days: workCards[i].delivery - workCards[i].commit
+        })
+      }
+    }
+    results = results.sort(function(a, b) {
+      return a.days - b.days
+    })
+    const data = {
       ids: [],
       effort: [],
       days: []
     }
-    for (let i = 0; i < workCards.length; i++) {
-      if (workCards[i].delivery) {
-        results.ids.push('#' + workCards[i].number)
-        results.effort.push(cardFuns.totalEffort(workCards[i]))
-        results.days.push(workCards[i].delivery - workCards[i].commit)
-      }
+    for (i = 0; i < results.length; i++) {
+      data.ids.push(results[i].id)
+      data.effort.push(results[i].effort)
+      data.days.push(results[i].days)
     }
-    return results
+    return data
   }
 }

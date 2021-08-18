b6e9f238c8edd1e5cab1a6ff9e44282b7a43da21
hogsmill <info@hogsmill.com>
2021-08-18 08:38:56 +0100

    deploy

--- deploy.sh
+++ deploy.sh
@@ -16,7 +16,6 @@ done
 REPO="https://github.com/hogsmill/no-estimates.git"
 APPS=(
   'no-estimates,noEstimatesGames,noEstimates,3007,No Estimates'
-  'no-estimates-game-test,noEstimatesGameTestGames,noEstimatesGameTest,3037,No Estimates,No Estimates Game Join Test'
   'no-estimates-funconf,noEstimatesFunconfGames,noEstimatesFunconf,3042,No Estimates,No Estimates - Funconf 2021'
   'no-estimates-new,noEstimatesNewGames,noEstimatesNew,3020,No Estimates,No Estimates Private'
   'no-estimates-dex,noEstimatesDexGames,noEstimatesDex,3051,No Estimates,No Estimates Dex'

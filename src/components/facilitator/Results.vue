eac154d91e25969fd60a75957dba75af4d0079d6
hogsmill <info@hogsmill.com>
2021-08-25 20:06:38 +0100

    debug

--- src/components/facilitator/Results.vue
+++ src/components/facilitator/Results.vue
@@ -508,6 +508,7 @@ export default {
         this.wip.data.labels = data.results.labels
         this.wip.data.datasets[0].data = data.results.wip
       }
+      console.log(this.wip.data)
       this.$modal.show('wip')
     },
     showCumulativeFlow(data) {

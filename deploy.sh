5122a24edc539ec01af215f2b65c5c10501a0d2f
hogsmill <info@hogsmill.com>
2021-09-05 17:31:50 +0100

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 FORCE=false
-OUTDATED=true
+NEW=false
 while [ $1 ]
 do
+  echo $1
   if [ "$1" == "-f" ]; then
     FORCE=true
+  fi
+  if [ "$1" == "-n" ]; then
+    NEW=true
   fi
   shift
 done
@@ -57,6 +61,11 @@ do
   fi
 
   cd $DIR
+  if [ "$NEW" == "true" ]
+  then
+    rm $DIR/package-lock.json
+    rm -rf $DIR/node_modules
+  fi
 
   PWD=`pwd`
   APP=`basename $PWD`

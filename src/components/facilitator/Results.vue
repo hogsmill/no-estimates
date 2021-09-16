1b07bbcd788b553758f11abae2949e2c5956f558
hogsmill <info@hogsmill.com>
2021-09-16 16:48:32 +0100

    debug

--- src/components/facilitator/Results.vue
+++ src/components/facilitator/Results.vue
@@ -531,7 +531,7 @@ export default {
     },
     showCorrelation(data) {
       console.log('correlation', data)
-      for (let i = 0, j = 1; i < Object.keys(this.selectedGraphTeams).length; i++. j++) {
+      for (let i = 0, j = 1; i < Object.keys(this.selectedGraphTeams).length; i++, j++) {
         if (this.selectedGraphTeams[j]) {
           this.correlations[j] = parseFloat(data.results[i])
           console.log(this.correlations)

62eeeb67de092caa1b86ac7a88555797cdeec887
hogsmill <info@hogsmill.com>
2021-12-15 14:07:02 +0000

    add Bud

--- deploy.sh
+++ deploy.sh
@@ -32,6 +32,7 @@ ROUTES=(
   'eagile','EverydayAgile','No Estimates'
   'and','And','No Estimates'
   'richemont','Richemont','No Estimates'
+  'bud','Bud','No Estimates'
   '','','Kanban Playground'
   'new','New','Kanban Playground'
   'dex','Dex','Kanban Playground'
@@ -41,6 +42,7 @@ ROUTES=(
   'eagile','EverydayAgile','Kanban Playground'
   'and','And','Kanban Playground'
   'richemont','Richemont','Kanban Playground'
+  'bud', 'Bud','Kanban Playground'
 )
 
 for ((i = 0; i < ${#ROUTES[@]}; i++))
